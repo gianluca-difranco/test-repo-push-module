ee1e53f7ef741dfed58247945831aa580cfddb4e
Gianluca Di Franco <g.difranco@innonation.it>
2026-02-06 17:26:44 +0100

    [ADD] scuffold_template_19_test: aggiunta nuovo modulo template per generazione moduli

---
+++ scuffold_template_19_test/models/models.py
@@ -0,0 +1,17 @@
+# from odoo import models, fields, api
+
+
+# class scuffold_template_19(models.Model):
+#     _name = 'scuffold_template_19.scuffold_template_19'
+#     _description = 'scuffold_template_19.scuffold_template_19'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
